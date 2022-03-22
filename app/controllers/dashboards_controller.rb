de0e02a5305637b99db3a3f712f9067e0382f80d
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-03-22 21:51:04 +0100

    placeholder for login

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,10 +1,9 @@
 class DashboardsController < ApplicationController
   def my_dashboard
     @my_bookings = Booking.my_bookings(current_user)
-    # byebug
+
     @received_bookings = Booking.received_bookings(current_user)
     @archived_bookings = Booking.archived_bookings(current_user)
-    # @my_dogs = Dog.where(user: current_user)
     @my_dogs = current_user.dogs
     @user = current_user
   end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,9 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
-  
-
   def home
     @dogs = Dog.all
   end
 
   def about_us
   end
-
 end
